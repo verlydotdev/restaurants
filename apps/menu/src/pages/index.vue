2cf9151e7d1766e6cc75a4b72b1f34b895365ef5
KFuee <jblascoisazac@icloud.com>
2023-01-08 17:34:16 +0100

    feat: maquetación básica tarjetas categorías

--- apps/menu/src/pages/index.vue
+++ apps/menu/src/pages/index.vue
@@ -11,6 +11,39 @@
       >
     </div>
   </div>
+
+  <!-- Categorías -->
+  <div class="indigo-500 mt-4 bg-sky-300 text-white">
+    <!-- Título y ver todo -->
+    <div class="flex items-center justify-between p-4">
+      <span class="text-2xl font-bold">Salsas clásicas</span>
+
+      <span class="text-sm">Ver todo</span>
+    </div>
+
+    <!-- Artículos -->
+    <div class="grid grid-cols-2 gap-4 px-4 pb-4">
+      <div class="flex flex-col">
+        <div
+          class="h-40 rounded-lg bg-white bg-[url('https://dam.cocinafacil.com.mx/wp-content/uploads/2014/11/pasta-pomodoro.jpg')] bg-cover bg-center p-4 shadow-lg"
+        ></div>
+
+        <span class="my-2 text-xl font-bold">Pomodoro</span>
+        <span class="text-sm text-black"
+          >Tomate italiano y un toque de albahaca...</span
+        >
+      </div>
+
+      <div class="flex flex-col">
+        <div
+          class="h-40 rounded-lg bg-white bg-[url('https://www.halfbakedharvest.com/wp-content/uploads/2021/12/My-Familys-Favorite-Pesto-Pasta-Bolognese-1.jpg')] bg-cover bg-center p-4 shadow-lg"
+        ></div>
+
+        <span class="my-2 text-xl font-bold">Bolognese</span>
+        <span class="text-sm text-black">Ragout auténtico de ternera</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
