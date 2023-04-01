b198203433e334021450b7d02f689aa0fd2b27d7
Jordi Gómez <gnugomez@gmail.com>
2023-04-01 22:15:48 +0200

    chore(menu): adding search input styles into header

--- apps/menu/src/app/shared/layouts/default-layout/ui/header/header.component.ts
+++ apps/menu/src/app/shared/layouts/default-layout/ui/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderInfoComponent } from '../header-info/header-info.component';
 import { IsOpenComponent } from '../is-open/is-open.component';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'restaurants-header',
   standalone: true,
-  imports: [CommonModule, HeaderInfoComponent, IsOpenComponent],
+  imports: [CommonModule, HeaderInfoComponent, IsOpenComponent, SearchComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
